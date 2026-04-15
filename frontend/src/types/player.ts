6bb8d95d38a79c13334e3aedd3139735058758e3
gokuthong <167153954+gokuthong@users.noreply.github.com>
2026-04-15 17:26:46 +0800

    auto: update player.ts

---
+++ frontend/src/types/player.ts
@@ -0,0 +1,20 @@
+export interface Player {
+  id: string;
+  name: string;
+  avatarUrl?: string;
+  createdBy?: string;
+  stats?: PlayerStats;
+  isGuest?: boolean;
+  userId?: string;
+  userNumber?: string;
+}
+
+export interface PlayerData extends Omit<Player, 'id'> {}
+
+export interface PlayerStats {
+  gamesPlayed: number;
+  totalPoints: number;
+  wins: number;
+  birdies: number;
+  eagles: number;
+}
